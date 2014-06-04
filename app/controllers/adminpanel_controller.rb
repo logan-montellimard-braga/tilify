fec6e2f28a742324c23e17f1694ab3621c61218c
Logan Braga <braga.logan@gmail.com>
2014-06-04 12:55:42 +0200

    Admin panel

--- app/controllers/adminpanel_controller.rb
+++ app/controllers/adminpanel_controller.rb
@@ -1,5 +1,10 @@
 class AdminpanelController < ApplicationController
   before_filter :authenticate_admin!
   def index
+    @users = User.all
+  end
+
+  def users
+    @users = User.all
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'adminpanel/index'
+  get 'adminpanel/users' => 'adminpanel#users'
 
   devise_for :admins
   root 'site#index'
