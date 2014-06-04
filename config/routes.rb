b4f430f6649ccde8a60c0841695e407cac5a3d84
Logan Braga <braga.logan@gmail.com>
2014-06-04 10:44:34 +0200

    No more profile

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get '/contact' => 'site#contact'
 
   devise_for :users
-  get 'users/:id' => 'users#show'
+  # get 'users/:id' => 'users#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
