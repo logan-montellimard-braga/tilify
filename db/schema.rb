d55b5a6d0d61c2e453aa6d2dd0d4fa291ea54543
Logan Braga <braga.logan@gmail.com>
2014-06-04 22:13:23 +0200

    Username for users and admins

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140604073329) do
+ActiveRecord::Schema.define(version: 20140604195740) do
 
   create_table "admins", force: true do |t|
     t.string   "email",                  default: "", null: false
@@ -26,10 +26,12 @@ ActiveRecord::Schema.define(version: 20140604073329) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "admins", ["email"], name: "index_admins_on_email", unique: true
   add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  add_index "admins", ["username"], name: "index_admins_on_username", unique: true
 
   create_table "users", force: true do |t|
     t.string   "email",               default: "", null: false
@@ -37,8 +39,10 @@ ActiveRecord::Schema.define(version: 20140604073329) do
     t.datetime "remember_created_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
