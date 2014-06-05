c016cfb6ac14e16e00a1c8f0d4aa69fd1b3bfe2e
Logan Braga <braga.logan@gmail.com>
2014-06-05 21:52:33 +0200

    Admin panel better

--- app/controllers/adminpanel_controller.rb
+++ app/controllers/adminpanel_controller.rb
@@ -4,10 +4,24 @@ class AdminpanelController < ApplicationController
     @users = User.all
     @admins = Admin.all
     @public_messages = Message.where(:public => true)
+    @public_messages = @public_messages.reverse
     @private_messages = Message.where(:public => false)
+    @private_messages = @private_messages.reverse
   end
 
   def users
     @users = User.all
+  end
+
+  def admins
+    @admins = Admin.all
+  end
+
+  def messages
+    @public_messages = Message.where(:public => true)
+    @private_messages = Message.where(:public => false)
+  end
+
+  def tiles
   end
 end
