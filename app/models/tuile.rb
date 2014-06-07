69d8d30c6ece276ce2f5163bfc361d3146ab1ce5
Logan Braga <braga.logan@gmail.com>
2014-06-08 00:32:34 +0200

    Tuile model

--- app/models/tuile.rb
+++ app/models/tuile.rb
@@ -1,3 +1,10 @@
 class Tuile < ActiveRecord::Base
   belongs_to :user
+
+  validates :lien, :format => URI::regexp(%w(http https)), presence: true
+  validates :image, :format => URI::regexp(%w(http https)), presence: true
+  validates :titre, :uniqueness => { :case_sensitive => false },
+    length: { minimum: 5, maximum: 50 }, presence: true
+  validates :description, length: { minimum: 10, maximum: 300 }, presence: true
+  validates :forme, presence: true
 end
