b9aa16e282586807db77da666a41d438d3e4f4b2
Logan Braga <braga.logan@gmail.com>
2014-06-08 11:49:22 +0200

    Tuiles type rendered

--- app/helpers/tuiles_helper.rb
+++ app/helpers/tuiles_helper.rb
@@ -1,2 +1,22 @@
 module TuilesHelper
+  def getIconByType(tuile)
+    case tuile.forme.downcase
+    when 'image'
+      return 'image'
+    when 'tutoriel'
+      return 'graduation-cap'
+    when 'vidéo'
+      return 'video-camera'
+    when 'site'
+      return 'bookmark'
+    when 'forum'
+      return 'comments'
+    when 'documentation'
+      return 'book'
+    when 'autre'
+      return 'ellipsis-h'
+    else
+      return 'ellipsis-h'
+    end
+  end
 end
